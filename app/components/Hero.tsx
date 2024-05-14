01b81b569fd507b4bcf0e5d500db3fbd278098f9
Adarsh L K <129508819+adarshlkdev@users.noreply.github.com>
2024-05-14 14:39:23 +0530

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -24,7 +24,7 @@ export default function Hero(){
                 simple yet powerful Kanban board.
               </p>
             </div>
-            <Link href={"/sign-up"}>
+            <Link href={"/onboarding"}>
               <Button normalButton text="Start planning now &#8594;"></Button>
             </Link>
           </div>
